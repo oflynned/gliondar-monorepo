736dbf9b94f01f928a4744bddbdf78b20abb8bc3
Edmond O'Flynn <oflynned@tcd.ie>
2023-04-22 21:21:37 +0200

    chore: create map component

---
+++ apps/webapp/design-system/components/Map/Map.tsx
@@ -0,0 +1,57 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import {
+  FunctionComponent,
+  PropsWithChildren,
+  useCallback,
+  useState,
+} from 'react';
+
+type Map = google.maps.Map;
+
+export const Map: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    // TODO move this to some config service
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  const [map, setMap] = useState<Map | null>(null);
+
+  const onLoad = useCallback((map: Map) => {
+    map.setOptions({
+      disableDoubleClickZoom: true,
+      zoomControl: false,
+      streetViewControl: false,
+      mapTypeControl: false,
+      fullscreenControl: false,
+      draggable: false,
+      clickableIcons: false,
+    });
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={{
+        width: '100%',
+        height: '100%',
+        borderRadius: 'inherit',
+      }}
+      center={{ lat: 53.343337, lng: -6.260073 }}
+      zoom={14}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {children}
+    </GoogleMap>
+  );
+};

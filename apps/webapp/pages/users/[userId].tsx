0b6d35d743e8d389a8338592625eb010d5155625
Edmond O'Flynn <oflynned@tcd.ie>
2023-04-22 15:43:48 +0200

    chore: move user profile

---
+++ apps/webapp/pages/users/[userId].tsx
@@ -0,0 +1,21 @@
+import { NavBarPage, SideBarLayout, TitledLayout } from '../../design-system';
+import { useRouter } from 'next/router';
+import { mockPeople } from '../../mock-data/mock-people';
+
+const Profile = () => {
+  const router = useRouter();
+  const { userId } = router.query;
+  const user = mockPeople.find((user) => user.id === userId);
+
+  return (
+    <SideBarLayout activePage={NavBarPage.PROFILE}>
+      <TitledLayout
+        pageTitle={user ? user.name : 'no user!'}
+        gap={4}
+        flex={2}
+      ></TitledLayout>
+    </SideBarLayout>
+  );
+};
+
+export default Profile;

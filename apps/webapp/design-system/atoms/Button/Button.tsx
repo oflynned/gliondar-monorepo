823ce7978d1fdd194329c0ab0efe7c84bd782203
Edmond O'Flynn <oflynned@tcd.ie>
2023-04-23 15:46:18 +0200

    chore: use button for open state

--- apps/webapp/design-system/atoms/Button/Button.tsx
+++ apps/webapp/design-system/atoms/Button/Button.tsx
@@ -5,11 +5,10 @@ import {
   Typography,
 } from '@mui/material';
 import { FunctionComponent } from 'react';
+import { Flex } from '../Flex';
 
 const StyledButton = styled(MuiButton)(({ theme }) => ({
   textTransform: 'none',
-  paddingX: theme.spacing(4),
-  paddingY: theme.spacing(1),
 }));
 
 type Props = ButtonProps & { label: string };
@@ -17,9 +16,12 @@ type Props = ButtonProps & { label: string };
 export const Button: FunctionComponent<Props> = ({ label, ...props }) => (
   <StyledButton
     variant={props.variant ?? 'outlined'}
+    size={props.size ?? 'large'}
     disableElevation
     {...props}
   >
-    <Typography variant={'h5'}>{label}</Typography>
+    <Flex width={'100%'}>
+      <Typography variant={'h5'}>{label}</Typography>
+    </Flex>
   </StyledButton>
 );

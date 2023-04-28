31df951f64a5df1c7b31ef6bc43ebc60f986b155
Edmond O'Flynn <oflynned@tcd.ie>
2023-04-28 10:24:57 +0200

    chore: add frontend timestamp formatters

---
+++ libs/fe/design-system/src/lib/formatters/timestamp-formatter.ts
@@ -0,0 +1,41 @@
+import { DateTime } from 'luxon';
+
+// TODO should account for locale here
+export const formatTimestampToDateTime = (timestamp: Date): string => {
+  // Wednesday, 10 May 2023
+  return DateTime.fromJSDate(timestamp).toLocaleString({
+    ...DateTime.DATE_FULL,
+    weekday: 'long',
+  });
+};
+
+export const formatTimestampToDate = (timestamp: Date): string => {
+  // 19 April 2023
+  return DateTime.fromJSDate(timestamp).toLocaleString(DateTime.DATE_FULL);
+};
+
+export const formatTimeElapsed = (timestamp: Date): string => {
+  console.log({ timestamp });
+
+  const referenceTime = DateTime.fromJSDate(timestamp);
+  const now = DateTime.now();
+
+  const minsElapsed = Math.floor(now.diff(referenceTime).as('minutes'));
+  const hoursElapsed = Math.floor(now.diff(referenceTime).as('hours'));
+
+  // console.log({ minsElapsed, hoursElapsed });
+
+  if (minsElapsed < 1) {
+    return 'Just now';
+  }
+
+  if (hoursElapsed < 1) {
+    return `${minsElapsed} mins ago`;
+  }
+
+  if (hoursElapsed < 24) {
+    return `${hoursElapsed} hours ago`;
+  }
+
+  return formatTimestampToDate(timestamp);
+};
